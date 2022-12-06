9b9db227375dda02ea7eed237fc4f4911964ab4a
sheogorath15 <48446789+sheogorath15@users.noreply.github.com>
2022-12-06 10:54:07 +0100

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_autocompleteactivities';
 $plugin->release = '0.2.0';
-$plugin->version = 2022111802;
+$plugin->version = 2022111803;
 $plugin->requires = 2020061514;
 $plugin->maturity = MATURITY_STABLE;
